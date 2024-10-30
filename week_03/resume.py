f080cd55ce3baf75a71e594b06c3913bac9b8bac
Chalantorn-TNK <67319010005@tnk.ac.th>
2024-10-30 18:23:53 +0700

    resume

---
+++ week_03/resume.py
@@ -0,0 +1,21 @@
+print('กรอกข้อมูล')
+name = str(input('โปรดกรอกชื่อ \n'))
+
+age = int(input('โปรดกรอกอายุ \n'))
+
+id = int(input('โปรดกรอกรหัสประจำตัว \n'))
+
+level = int(input('โปรดกรอกระดับชั้น \n'))
+
+nickname = str(input('โปรดกรอกชื่อเล่น \n'))
+
+
+height = float(input('โปรดกรอกส่วนสูง \n'))
+
+weight = float(input('โปรดกรอกน้ำหนัก \n'))
+
+print('\nชื่อ: ' + name +  ' อายุ: '  +str (age) + ' ปี')
+print('รหัสประจำตัวนักเรียน: ' +  str (id)   + ' ระดับชั้น: ' + str(level))
+print('ชื่อเล่น: ' +  nickname)
+print('ส่วนสูง: ' + str (height) + ' เซนติเมตร' + ' น้ำหนัก: ' + str (weight) +' กิโลกรัม')
+print('ส่วนสูง + น้ำหนัก = ' + str(height + weight))
